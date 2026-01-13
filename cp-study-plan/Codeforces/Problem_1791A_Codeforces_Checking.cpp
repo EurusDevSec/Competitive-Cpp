73579ecf3eb99c7c1e5b970231285e8be3336bca
LeganSar44 <hoangslevan@gmail.com>
2026-01-13 14:04:09 +0700

    add Problem 1791A - Codeforces Checking

---
+++ cp-study-plan/Codeforces/Problem_1791A_Codeforces_Checking.cpp
@@ -0,0 +1,39 @@
+//
+// Created by ACER on 13/01/2026.
+//
+
+#include <bits/stdc++.h>
+
+using namespace  std;
+
+
+void solve() {
+    char c;
+    cin>>c;
+    string target = "codeforces";
+
+   if (c=='c' || c=='o' || c == 'd' || c=='e' || c=='f' || c =='r' || c=='s') {
+       cout<<"YES"<<"\n";
+   }
+   else {
+        cout<<"NO"<<"\n";
+    }
+
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+
+
+    while (t--) {
+      solve();
+
+    }
+
+
+    return 0;
+}
