bdbd029235b5fe6f9eb7808119c7b0caa4235aa3
LeganSar44 <hoangslevan@gmail.com>
2026-01-14 18:27:31 +0700

    add Problem 1 - Two Sum solution

---
+++ cp-study-plan/LeetCode/Blind 75/1_TwoSum.cpp
@@ -0,0 +1,38 @@
+//
+// Created by ACER on 13/01/2026.
+//
+#include  <vector>
+#include <unordered_map>
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int>twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int>numMap;
+
+        for (int i=0;i<nums.size();++i) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]]=i;
+        }
+        return {};
+    }
+};
+
+int main() {
+   Solution solution;
+    vector<int>nums= {2,7,11,15};
+    int target = 9;
+
+    vector<int>result = solution.twoSum(nums, target);
+    if (!result.empty()) {
+        cout<<result[0]<<", "<<result[1]<<endl;
+    }
+
+    return 0;
+}
