df2adb8941dea21f2b495b6dcd3ed7d8009de0f1
LeganSar44 <hoangslevan@gmail.com>
2026-01-15 10:56:48 +0700

    fix Valid Parentheses logic to correctly handle all closing brackets

---
+++ cp-study-plan/LeetCode/Blind 75/125_ValidPalindrome.cpp
@@ -0,0 +1,44 @@
+//
+// Created by ACER on 15/01/2026.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class  Solution {
+public:
+    bool isPalindrome(string s) {
+
+    string fw = remove_non_alpha(s);
+    string bw = fw;
+        reverse(bw.begin(),bw.end());
+        return fw == bw ;
+
+
+    }
+    string remove_non_alpha(string s) {
+
+        string t = "";
+        for (char c : s) {
+            if (std::isalnum(c)) {
+                t+=std::tolower(c);
+            }
+        }
+        return t;
+    }
+};
+
+
+
+int main() {
+    Solution solution;
+    string s = "0P";
+    if (solution.isPalindrome(s)) {
+        cout<<"true";
+
+    }
+    else cout<<"false";
+
+    return 0;
+}
