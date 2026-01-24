a2f1b50053a78ba6c77076b9eca6a4c65c4aeff7
LeganSar44 <hoangslevan@gmail.com>
2026-01-24 22:09:51 +0700

    feat: add initial implementation for Merge Sorted Array problem in 88_Merge_Sorted_Array.cpp

---
+++ cp-study-plan/LeetCode/88_Merge_Sorted_Array.cpp
@@ -0,0 +1,32 @@
+//
+// Created by ACER on 23/01/2026.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace  std;
+
+
+class  Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i = m-1;
+        int j = n-1;
+        int k = m+n -1;
+
+
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<int>nums1 = {1,2,3,0,0,0};
+    vector<int>nums2 = {2,5,6};
+    int m=3,n=3;
+
+
+    return 0;
+}
